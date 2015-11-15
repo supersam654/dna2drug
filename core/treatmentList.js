897847c0dce5866e8924dfa486520eca6d2a12c2
kurtcarpenter <kurtcarpenter@hotmail.com>
2015-11-15 05:08:56 -0500

    treatment list

---
+++ core/treatmentList.js
@@ -0,0 +1,14 @@
+var treatmentList;
+treatmentList.Geftinib = {
+  "name": "Geftinib",
+  "treats": "EGFR",
+  "url": "http://www.sciencemag.org/content/305/5687/1163",
+  "description": "Gefitinib is the first selective inhibitor of epidermal growth factor receptor's (EGFR) tyrosine kinase domain. Thus gefitinib is an EGFR inhibitor. The target protein (EGFR) is a family of receptors which includes Her1(erb-B1), Her2(erb-B2), and Her 3(erb-B3). EGFR is overexpressed in the cells of certain types of human carcinomas - for example in lung and breast cancers. This leads to inappropriate activation of the anti-apoptotic Ras signalling cascade, eventually leading to uncontrolled cell proliferation. Research on gefitinib-sensitive non-small cell lung cancers has shown that a mutation in the EGFR tyrosine kinase domain is responsible for activating anti-apoptotic pathways. These mutations tend to confer increased sensitivity to tyrosine kinase inhibitors such as gefitinib and erlotinib. Of the types of non-small cell lung cancer histologies, adenocarcinoma is the type that most often harbors these mutations. These mutations are more commonly seen in Asians, women, and non-smokers (who also tend to more often have adenocarcinoma).¶ Gefitinib inhibits EGFR tyrosine kinase by binding to the adenosine triphosphate (ATP)-binding site of the enzyme. Thus the function of the EGFR tyrosine kinase in activating the anti-apoptotic Ras signal transduction cascade is inhibited, and malignant cells are inhibited."
+}
+
+treatmentList.Cetuximab = {
+  "name": "",
+  "treats": "",
+  "url": "",
+  "description"
+}
